cb62abfdb2f3a3b52ef71741ad48ca1dc937d517
Augustosullver <158534817+Augustosullver@users.noreply.github.com>
2025-08-22 12:30:04 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Discord, Trophy, User, Info } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
